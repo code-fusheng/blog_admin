84ea83a115de303e478dee3f7f00eb5660e1d00c
zhfushengx2048 <53218036+zhfushengx2048@users.noreply.github.com>
2020-03-20 18:16:42 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- 路由占位符 -->
+    <router-view />
+    <div />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+
+</script>
+<style>
+.add-button {
+  margin-bottom: 15px;
+}
+.pagination {
+  margin-top: 15px;
+}
+</style>
